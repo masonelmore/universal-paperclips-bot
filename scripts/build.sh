3fca2f7004ba11ea1b01ff49ef047c564a67b612
Mason Elmore <masonelmore@users.noreply.github.com>
2025-10-28 21:30:00 -0500

    Bundle bot scripts into a single file (#7)
    Simplifies managing script dependencies. Don't need to worry about order when they're all in the same file.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Building..."
+echo -n "Building..."
 
+# Copy game files
 mkdir -p build
 cp -r game/* build
-cp -r bot build/bot
+
+# Bundle and copy bot files
+{
+    for file in bot/*.js; do
+        echo "// FILE: $(basename "$file")"
+        cat "$file"
+        echo ""
+    done
+} > build/bot.bundle.js
+
+echo "done"
